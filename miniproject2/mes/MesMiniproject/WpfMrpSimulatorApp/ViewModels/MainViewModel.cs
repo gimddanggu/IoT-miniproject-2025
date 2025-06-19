2fd9ac1f9adb1e7258052616decb630ff516ae97
dahyun_0_1 <changum0520@gmail.com>
2025-06-19 12:33:53 +0900

    미니프로젝트 4일차

---
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfMrpSimulatorApp.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private string _greeting;
+
+        public MainViewModel()
+        {
+            Greeting = "MRP 공정관리!";
+        }
+        public string Greeting
+        {
+            get => _greeting;
+            set => SetProperty(ref _greeting, value);   
+        }
+    }
+}
--- miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/Views/MainView.xaml.cs
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/Views/MainView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
-namespace WpfMrpSimulatorApp
+namespace WpfMrpSimulatorApp.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainView : MetroWindow
     {
-        public MainWindow()
+        public MainView()
         {
             InitializeComponent();
         }
